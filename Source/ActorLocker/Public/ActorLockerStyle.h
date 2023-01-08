d5d0b416c6d8af445bac8becf72ac76d32dbe42f
Gradess <trofimovjava@gmail.com>
2023-01-08 14:00:23 +0100

    [AC-1] Add ActorLockerStyle

---
+++ Source/ActorLocker/Public/ActorLockerStyle.h
@@ -0,0 +1,28 @@
+﻿// Copyright Gradess Games. All Rights Reserved.
+
+#pragma once
+#include "CoreMinimal.h"
+
+class ISlateStyle;
+class FSlateStyleSet;
+
+/**
+ * Actor Locker style that defines icon locations, style brushes, etc.
+ */
+class FActorLockerStyle
+{
+private:
+	static TSharedPtr<FSlateStyleSet> StyleInstance;
+	
+public:
+	static void Initialize();
+	static void Shutdown();
+	static void ReloadTextures();
+	static const ISlateStyle& Get();
+	static FName GetStyleSetName();
+
+private:
+	static TSharedRef<FSlateStyleSet> Create();
+};
+
+
