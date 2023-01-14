4a45fc445a3b71617f8eaa386544f28210b6cff9
Gradess <trofimovjava@gmail.com>
2023-01-14 20:11:54 +0100

    [AC-6] Fix key bindings

--- Source/ActorLocker/Private/ActorLockerCommands.cpp
+++ Source/ActorLocker/Private/ActorLockerCommands.cpp
@@ -6,11 +6,11 @@
 
 void FActorLockerCommands::RegisterCommands()
 {
-	UI_COMMAND(LockObject, "LockObject", "Lock selected object", EUserInterfaceActionType::Button, FInputChord(EKeys::L, EModifierKey::Alt));
-	UI_COMMAND(UnlockObject, "UnlockObject", "Unlock selected object", EUserInterfaceActionType::Button, FInputChord(EKeys::U, EModifierKey::Alt));
-	UI_COMMAND(LockAllObjects, "LockAllObjects", "Lock all selected objects", EUserInterfaceActionType::Button, FInputChord(EKeys::L, EModifierKey::Alt | EModifierKey::Shift));
-	UI_COMMAND(UnlockAllObjects, "UnlockAllObjects", "Unlock all selected objects", EUserInterfaceActionType::Button, FInputChord(EKeys::U, EModifierKey::Alt | EModifierKey::Shift));
-	UI_COMMAND(ToggleLockedObjects, "ToggleLockedObjects", "Toggle state of locked objects", EUserInterfaceActionType::Button, FInputChord(EKeys::T, EModifierKey::Control));
+	UI_COMMAND(LockObject, "LockObject", "Lock selected object", EUserInterfaceActionType::Button, FInputChord(EKeys::Comma, EModifierKey::Alt));
+	UI_COMMAND(UnlockObject, "UnlockObject", "Unlock selected object", EUserInterfaceActionType::Button, FInputChord(EKeys::Period, EModifierKey::Alt));
+	UI_COMMAND(LockAllObjects, "LockAllObjects", "Lock all selected objects", EUserInterfaceActionType::Button, FInputChord(EKeys::Comma, EModifierKey::Alt | EModifierKey::Shift));
+	UI_COMMAND(UnlockAllObjects, "UnlockAllObjects", "Unlock all selected objects", EUserInterfaceActionType::Button, FInputChord(EKeys::Period, EModifierKey::Alt | EModifierKey::Shift));
+	UI_COMMAND(ToggleLockedObjects, "ToggleLockedObjects", "Toggle state of locked objects", EUserInterfaceActionType::Button, FInputChord(EKeys::M, EModifierKey::Alt));
 }
 
 #undef LOCTEXT_NAMESPACE
