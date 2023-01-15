9d2fedbfbea7bb1b766aba95850db17c933176c1
Gradess <trofimovjava@gmail.com>
2023-01-15 04:45:04 +0100

    [AC-10] Add ActorLockerSettings

--- Source/ActorLocker/Private/ActorLockerCommands.cpp
+++ Source/ActorLocker/Private/ActorLockerCommands.cpp
@@ -1,16 +1,18 @@
 ﻿// Copyright Gradess Games. All Rights Reserved.
 
 #include "ActorLockerCommands.h"
+#include "ActorLockerSettings.h"
 
 #define LOCTEXT_NAMESPACE "ActorLockerPlugin"
 
 void FActorLockerCommands::RegisterCommands()
 {
-	UI_COMMAND(LockObject, "LockObject", "Lock selected object", EUserInterfaceActionType::Button, FInputChord(EKeys::Comma, EModifierKey::Alt));
-	UI_COMMAND(UnlockObject, "UnlockObject", "Unlock selected object", EUserInterfaceActionType::Button, FInputChord(EKeys::Period, EModifierKey::Alt));
-	UI_COMMAND(LockAllObjects, "LockAllObjects", "Lock all selected objects", EUserInterfaceActionType::Button, FInputChord(EKeys::Comma, EModifierKey::Alt | EModifierKey::Shift));
-	UI_COMMAND(UnlockAllObjects, "UnlockAllObjects", "Unlock all selected objects", EUserInterfaceActionType::Button, FInputChord(EKeys::Period, EModifierKey::Alt | EModifierKey::Shift));
-	UI_COMMAND(ToggleLockedObjects, "ToggleLockedObjects", "Toggle state of locked objects", EUserInterfaceActionType::Button, FInputChord(EKeys::M, EModifierKey::Alt));
+	const auto ActorLockerSettings = GetDefault<UActorLockerSettings>();
+	UI_COMMAND(LockObject, "LockObject", "Lock selected object", EUserInterfaceActionType::Button, ActorLockerSettings->LockObject);
+	UI_COMMAND(UnlockObject, "UnlockObject", "Unlock selected object", EUserInterfaceActionType::Button, ActorLockerSettings->UnlockObject);
+	UI_COMMAND(LockAllObjects, "LockAllObjects", "Lock all selected objects", EUserInterfaceActionType::Button, ActorLockerSettings->LockAllObjects);
+	UI_COMMAND(UnlockAllObjects, "UnlockAllObjects", "Unlock all selected objects", EUserInterfaceActionType::Button, ActorLockerSettings->UnlockAllObjects);
+	UI_COMMAND(ToggleLockedObjects, "ToggleLockedObjects", "Toggle state of locked objects", EUserInterfaceActionType::Button, ActorLockerSettings->ToggleLockedObjects);
 }
 
 #undef LOCTEXT_NAMESPACE
